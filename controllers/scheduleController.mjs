c019756e725f8ca8778482c976f4cb3b31098ce5
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-06-03 21:53:25 +0700

    update schedule API

--- controllers/scheduleController.mjs
+++ controllers/scheduleController.mjs
@@ -26,7 +26,7 @@ const createSchedule = (req, res) => {
 
   pool.query(
     'INSERT INTO schedules (id, date, time, is_available ,counselor_id) VALUES ($1, $2, $3, $4 ,$5)',
-    [schedule_id, student_id, status, created_at], (error, results) => {
+    [id, date, time, is_available ,counselor_id], (error, results) => {
     if (error) {
       throw error
     }
