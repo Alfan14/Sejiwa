80a929299ec8e71c14c4266bdaf899c95f397553
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-06-04 08:39:32 +0700

    update schedule API

--- controllers/scheduleController.mjs
+++ controllers/scheduleController.mjs
@@ -1,8 +1,8 @@
 import pool from "../db/index.mjs";
 
 const getSchedules = (req, res, next) => {
-    
-    pool.query('SELECT * FROM schedules ORDER BY id ASC', (error, results) => {
+
+  pool.query('SELECT * FROM schedules ORDER BY id ASC', (error, results) => {
     if (error) {
       throw error
     }
@@ -37,22 +37,22 @@ const createSchedule = (req, res) => {
   );
 };
 
-const updateSchedule = (req, res, next) => {
-  const id = parseInt(req.params.id)
-  const {date, time, is_available ,counselor_id } = req.body
+const updateSchedule = async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { date, time, is_available, counselor_id } = req.body;
 
-  pool.query(
-    'UPDATE schedules SET date = $1, time = $2, is_available = $3, counselor_id = $4 WHERE id = $5',
-    [ date, time, is_available ,counselor_id],
-    (error, results) => {
-      if (error) {
-        console.error('Database insert error:', error.message, error.stack);
-        return res.status(500).json({ error: 'Database error' });
-      }
-      res.status(201).send(`Schedule added with ID: ${results.rows[0].id}`);
-    }
-  )
+  try {
+    await pool.query(
+      'UPDATE schedules SET date = $1, time = $2, is_available = $3, counselor_id = $4 WHERE id = $5',
+      [date, time, is_available, counselor_id, id]
+    );
+    res.status(200).send(`Schedule modified with ID: ${id}`);
+  } catch (error) {
+    console.error('Database update error:', error.message);
+    res.status(500).json({ error: 'Database error' });
+  }
 };
+
 
 const deleteSchedule = (req, res) => {
   const id = parseInt(req.params.id)
