d475dc4a6d1776ef8836a7d9c7e0c83b250ca07a
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-06-08 08:18:30 +0700

    update controller

--- routes/api/bookingsRoute.mjs
+++ routes/api/bookingsRoute.mjs
@@ -6,10 +6,10 @@ const { authenticate, authorize } = authMiddleware
 
 const router = express.Router()
 
-router.get('/bookings',authenticate, authorize(['konselor']) , db_booking.getBookings);
-router.get('/bookings/:id',authenticate, authorize(['konselor']), db_booking.getBookingById);
-router.post('/bookings', authenticate, authorize(['pelajar']),db_booking.createBooking);
-router.put('/bookings/:id', authenticate, authorize(['pelajar']),db_booking.updateBooking);
-router.delete('/bookings/:id',authenticate, authorize(['pelajar']), db_booking.deleteBooking);
+router.get('/bookings',authenticate, authorize(['konselor','pelajar','admin']) , db_booking.getBookings);
+router.get('/bookings/:id',authenticate, authorize(['konselor','pelajar','admin']), db_booking.getBookingById);
+router.post('/bookings', authenticate, authorize(['konselor','pelajar','admin']),db_booking.createBooking);
+router.put('/bookings/:id', authenticate, authorize(['konselor','pelajar','admin']),db_booking.updateBooking);
+router.delete('/bookings/:id',authenticate, authorize(['admin']), db_booking.deleteBooking);
 
 export default router;
--- routes/api/consultationsRoute.mjs
+++ routes/api/consultationsRoute.mjs
@@ -6,10 +6,10 @@ const { authenticate, authorize } = authMiddleware
 
 const router = express.Router()
 
-router.get('/consultations', authenticate, authorize(['konselor']),db_consultation.getConsultations);
-router.get('/consultations/:id', authenticate, authorize(['konselor']),db_consultation.getConsultationById);
-router.post('/consultations', authenticate, authorize(['konselor']),db_consultation.createConsultation);
-router.put('/consultations/:id', authenticate, authorize(['konselor']), db_consultation.updateConsultation);
-router.delete('/consultations/:id', authenticate, authorize(['konselor']), db_consultation.deleteConsultation);
+router.get('/consultations', authenticate, authorize(['konselor','pelajar','admin']),db_consultation.getConsultations);
+router.get('/consultations/:id', authenticate, authorize(['konselor','pelajar','admin']),db_consultation.getConsultationById);
+router.post('/consultations', authenticate, authorize(['konselor','admin']),db_consultation.createConsultation);
+router.put('/consultations/:id', authenticate, authorize(['konselor','admin']), db_consultation.updateConsultation);
+router.delete('/consultations/:id', authenticate, authorize(['admin']), db_consultation.deleteConsultation);
 
 export default router;
