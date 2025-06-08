21791ee150f12701d9c1ce830c9f8d29e40c1bb3
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-06-09 06:39:12 +0700

    update assessmentController

--- routes/api/assesmentRoute.mjs
+++ routes/api/assesmentRoute.mjs
@@ -7,8 +7,8 @@ const { authenticate, authorize } = authMiddleware
 const router = express.Router()
 
 router.post('/assessment/submit', assessmentController.submitAssessment);
-router.get('/assessment/answer', assessmentController.saveAssessmentAnswer);
-router.post('/assessment/questions', assessmentController.getQuestions);
+router.post('/assessment/answer', assessmentController.saveAssessmentAnswer);
+router.get('/assessment/questions', assessmentController.getQuestions);
 
 export default router;
 
