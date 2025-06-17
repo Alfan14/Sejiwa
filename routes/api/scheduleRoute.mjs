871825c9b5fbc0c7c6027c83547a884188d757df
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-06-17 18:34:13 +0700

    update scheduleRoute

--- routes/api/scheduleRoute.mjs
+++ routes/api/scheduleRoute.mjs
@@ -9,7 +9,7 @@ const router = express.Router()
 router.get('/schedules',authenticate, authorize(['konselor','pelajar']) , db_schedule.getSchedules);
 router.get('/schedules/:id',authenticate, authorize(['konselor','pelajar']), db_schedule.getScheduleById);
 router.post('/schedules', authenticate, authorize(['konselor']),db_schedule.createSchedule);
-router.put('/schedules/:id', authenticate, authorize(['konselor']),db_schedule. updateScheduleByCounselorId);
+router.put('/schedules/', authenticate, authorize(['konselor']),db_schedule. updateScheduleByCounselorId);
 router.delete('/schedules/:id',authenticate, authorize(['konselor']), db_schedule.deleteSchedule);
 
 export default router;
