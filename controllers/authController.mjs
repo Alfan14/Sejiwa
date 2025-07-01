3792780eb9386f5280e6b7888cb64e0938a8ad1d
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-07-01 19:28:10 +0700

    update authController

--- controllers/authController.mjs
+++ controllers/authController.mjs
@@ -3,43 +3,38 @@ import jwt from "jsonwebtoken";
 import { defaults } from "pg";
 import db from "../models/index.mjs";
 
-// Calling a secret key
 const SECRET_KEY = process.env.SECRET_KEY;
 
-// Assigning users to the variable User
 const User = db.users;
 
-//  signing a user up
-//  hashing users password before its saved to the database with bcrypt
 const register = async (req, res) => {
-  try {
-    const { username, email, password, role, profile_picture } = req.body;
-    const data = {
-      username,
-      email,
-      password: await bcrypt.hash(password, 10),
-      role,
-      profile_picture
-    };
-    // saving the user
-    const user = await User.create(data);
+ try {
+   const { username, email, password , role , profile_picture } = req.body;
+   const data = {
+     username,
+     email,
+     password: await bcrypt.hash(password, 10),
+     role,
+     profile_picture 
+   };
 
-    if (user) {
-      let token = jwt.sign({ id: user.id }, SECRET_KEY, {
-        expiresIn: 1 * 24 * 60 * 60 * 1000,
-      });
+   const user = await User.create(data);
 
-      res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
-      console.log("user", JSON.stringify(user, null, 2));
-      console.log(token);
-      //send users details
-      return res.status(201).send(user);
-    } else {
-      return res.status(409).send("Details are not correct");
-    }
-  } catch (error) {
-    console.log(error);
-  }
+   if (user) {
+     let token = jwt.sign({ id: user.id },SECRET_KEY, {
+       expiresIn: 1 * 24 * 60 * 60 * 1000,
+     });
+
+     res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
+     console.log("user", JSON.stringify(user, null, 2));
+     console.log(token);
+     return res.status(201).send(user);
+   } else {
+     return res.status(409).send("Details are not correct");
+   }
+ } catch (error) {
+   console.log(error);
+ }
 };
 
 //  login authentication
